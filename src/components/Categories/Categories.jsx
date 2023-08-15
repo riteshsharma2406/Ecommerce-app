c880cde546dde1f5bdf11f04c4d6bc9167fbc8bb
Ritesh <101566115+riteshsharma2406@users.noreply.github.com>
2023-08-16 00:55:31 +0530

    Added two section of Featured and trending product using a card component

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Categories.scss"
+
+const Categories = () => {
+  return (
+    <div className='categories'>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col col-l"></div>
+    </div>
+  )
+}
+
+export default Categories
---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import "./FeaturedProducts.scss";
+import Card from './Card/Card';
+
+const FeaturedProducts = ({type}) => {
+
+    const data = [
+
+        {
+
+            id: 1,
+            img: "https://images.pexels.com/photos/6076066/pexels-photo-6076066.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            img2:"https://images.pexels.com/photos/6076065/pexels-photo-6076065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title:"White crop top and short",
+            isNew:true,
+            oldPrice:3999,
+            newPrice:2999,
+        },
+
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/1103828/pexels-photo-1103828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title:"Crew-neck shirt and short",
+            isNew:true,
+            oldPrice:3999,
+            newPrice:2999 
+        },
+
+        {
+            id: 3,
+            img: "https://images.pexels.com/photos/1007018/pexels-photo-1007018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title:"White Long-sleeved Shirt and Blue Skirt",
+            oldPrice:3999,
+            newPrice:2999 
+        },
+
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/11049240/pexels-photo-11049240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title:"Long Sleeve Hoodie",
+            oldPrice:3999,
+            newPrice:2999
+        },
+
+    ];
+
+  return (
+    <div className='featuredProducts'>
+      <div className="top">
+        <h1>{type} Products</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga excepturi ipsum voluptatibus at, quis laboriosam sint voluptate optio eaque aperiam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempora quas beatae eaque, possimus mollitia!</p>
+      </div>
+      <div className="bottom">
+        {data.map(item=>(
+            <Card item ={item} key={item.id}/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedProducts
